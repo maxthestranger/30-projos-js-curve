83e9d5271ece5a1d7e4035eb35a6b022847fa554
maxthestranger <thestrangermax@gmail.com>
2021-10-14 22:17:40 +0300

    refactor

--- form_validation/app.js
+++ form_validation/app.js
@@ -1,156 +1,113 @@
-// //  selector
-// const inputElement = document.querySelectorAll('input');
-// const errorMessage = document.createElement('p');
-// const submitBtn = document.querySelector('button');
-// errorMessage.classList.add('message');
-// const mailRegex =
-//   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-// const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-
-// let currentPassword = null;
-
-// inputElement.forEach((input) => {
-//   input.addEventListener('change', function () {
-//     if (input.value === '') {
-//       input.classList.add('error');
-//       errorMessage.innerText = `Value is required`;
-//       input.parentElement.appendChild(errorMessage);
-//     }
-
-//     if (input.name === 'username') {
-//       if (input.value.match(/\s|\W/gi)) {
-//         errorMessage.innerText = 'Only a-z0-9 and _ values allowed';
-//         input.parentElement.appendChild(errorMessage);
-//       } else {
-//         console.log(input.value);
-//       }
-//     }
-
-//     if (input.name === 'email') {
-//       if (mailRegex.test(input.value)) {
-//         console.log(input.value);
-//       } else {
-//         errorMessage.innerText = 'Enter a valid email';
-//         input.parentElement.appendChild(errorMessage);
-//       }
-//     }
-
-//     if (input.name === 'password') {
-//       if (passwordRegex.test(input.value)) {
-//         currentPassword = input.value;
-//       } else {
-//         errorMessage.innerText =
-//           'Passsword be 8 characters long, conatin at least one uppercase letter, symbol and number';
-//         input.parentElement.appendChild(errorMessage);
-//       }
-//     }
-
-//     if (input.name === 'confirm_password') {
-//       if (input.value === currentPassword) {
-//         console.log(currentPassword);
-//       } else {
-//         errorMessage.innerText = 'Password must match';
-//         input.parentElement.appendChild(errorMessage);
-//       }
-//     }
-//   });
-// });
-
-// Refactoring
-
 class Validation {
   constructor() {
     this.inputElements = document.querySelectorAll('input');
-    this.errorMessage = document.createElement('p').classList.add('message');
-    this.submitBtn = document.querySelector('button');
+    this.errorMessage = document.createElement('p');
+    this.form = document.querySelector('.card');
     this.mailRegex =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const passwordRegex =
-      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
     this.passwordRegex =
-      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
     this.currentPassword = null;
-    this.currentUser = {};
+    // this.currentUser = {};
   }
 
   //   Methods
 
   // Error Reporting
-  assignErrors(message) {
-    errorMessage.innerText = message;
-    input.parentElement.appendChild(errorMessage);
+  assignErrors(message, self) {
+    this.errorMessage.innerText = message;
+    this.errorMessage.classList.add('message');
+    self.parentElement.appendChild(this.errorMessage);
   }
 
-  validateInputs(self, inputName) {
+  // validate username
+  validateUsername(self) {
+    if (self.name === 'username') {
+      if (self.value.match(/\s|\W/gi)) {
+        this.assignErrors('Only a-z0-9 and _ values allowed', self);
+      } else {
+        // this.currentUser.username = self.value;
+        this.errorMessage.innerText = '';
+        console.log(self.value);
+      }
+    }
+  }
+
+  // validate email
+  validateEmail(self) {
+    if (self.name === 'email') {
+      if (this.mailRegex.test(self.value)) {
+        this.errorMessage.innerText = '';
+        console.log(self.value);
+      } else {
+        this.assignErrors('Enter a valid email', self);
+      }
+    }
+  }
+
+  validatePassword(self) {
+    if (self.name === 'password') {
+      if (this.passwordRegex.test(self.value)) {
+        // this.currentUser.password = self.value;
+        this.errorMessage.innerText = '';
+        this.currentPassword = self.value;
+        console.log(self.value);
+      } else {
+        this.assignErrors(
+          'Passsword be 8 characters long, conatin at least one uppercase letter, symbol and number',
+          self
+        );
+      }
+    }
+  }
+
+  confirmPassword(self) {
+    if (self.name === 'confirm_password') {
+      if (self.value === this.currentPassword) {
+        // this.currentUser.confirmed_password = self.value;
+        this.errorMessage.innerText = '';
+        console.log(self.value);
+      } else {
+        this.assignErrors('Password must match', self);
+      }
+    }
+  }
+
+  checkEmpty(self) {
+    if (self.value === '') {
+      self.classList.add('error');
+    } else {
+      self.classList.remove('error');
+    }
+  }
+
+  validateInputs(self) {
     //   Check if values are empty
-    (function () {
-      if (self.value === '') {
-        self.classList.add('error');
-      }
-    })();
-
-    // validate username
-    const validateUsername = () => {
-      if (inputName === 'username') {
-        if (self.value.match(/\s|\W/gi)) {
-          this.assignErrors('Only a-z0-9 and _ values allowed');
-        } else {
-          this.currentUser.username = self.value;
-        }
-      }
-    };
-
+    this.checkEmpty(self);
+    // validate usernameerrorMessage
+    this.validateUsername(self);
     // validate email
-    const validateEmail = () => {
-      if (inputName === 'email') {
-        if (this.mailRegex.test(self.value)) {
-          this.currentUser.email = self.value;
-        } else {
-          this.assignErrors('Enter a valid email');
-        }
-      }
-    };
-
+    this.validateEmail(self);
     // validate password
-    const validatePassword = () => {
-      if (inputName === 'password') {
-        if (this.passwordRegex.test(self.value)) {
-          this.currentUser.password = self.value;
-        } else {
-          this.assignErrors(
-            'Passsword be 8 characters long, conatin at least one uppercase letter, symbol and number'
-          );
-        }
-      }
-    };
-
+    this.validatePassword(self);
     // confirm password
-    const confirmPassword = () => {
-      if (inputName === 'password') {
-        if (self.value === currentPassword) {
-          this.currentUser.confirmed_password = self.value;
-        } else {
-          this.assignErrors('Password must match');
-        }
-      }
-    };
+    this.confirmPassword(self);
   }
 }
 
 const validation = new Validation();
 
 // Events
-validation.submitBtn.addEventListener('click', (e) => {
+validation.form.addEventListener('submit', (e) => {
   e.preventDefault();
+
   validation.inputElements.forEach((input) => {
-    input.addEventListener('change', function () {
-      validation.validateInputs(this, input.name);
-    });
+    validation.validateInputs(input);
   });
 });
 
 validation.inputElements.forEach((input) => {
   input.addEventListener('change', function () {
-    validation.validateInputs(this, input.name);
+    validation.validateInputs(this);
   });
 });
